590a3a902421374bb2b2ff0ae4e79744f8884eb3
KarinaKhainatska <hajnackakarina@gmail.com>
2025-10-31 23:36:10 +0200

    Delete newphp directory

--- newphp/pages/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <?php foreach ($pages as $path => $data): ?>
-        <a href="<?= htmlspecialchars($path) ?>">
-            <?= htmlspecialchars($data["title"]) ?>
-        </a>
-    <?php endforeach; ?>
-</nav>
